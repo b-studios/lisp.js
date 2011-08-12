c96765d57b3bdeca8c167a5c40a3b1ed262264fa
Jonathan <jonathan@b-studios.de>
2011-08-12 15:29:10 +0200

    Improved exception handling with call/cc. Now exceptions are checked and only caught, if exception-type is the same

--- tests/samples/exception_handling.lisp
+++ tests/samples/exception_handling.lisp
@@ -1,21 +1,39 @@
-(define try-catch (lambda (try-block catch-block)
-  (let ( (exception nil) )
-    (set! exception (call/cc try-block))
-    (if (eq? exception  nil)
-      (print "Nothing") ; do nothing
-    ; else
-      (catch-block exception)
-    )
+(defmacro throw (exception-type)
+  '(throw-exception (quote ,exception-type)))
+
+(defmacro try (try-block catch-block)
+
+  ; try muss sich das äußere environment merken, um dort 'throw' aufzurufen, wenn die Exception nicht gefangen werden kann
+  '(let ( (super-throw (if (defined? 'throw-exception) throw-exception (lambda (e) (error (+ "Not caught exception: " e)))))
+          (exception nil) )
+     (set! exception (call/cc (lambda (throw-exception) ,try-block)))
+     (if (eq? exception nil)
+        nil ; do nothing
+     ;else
+        (,catch-block exception))))     
+
+
+(defmacro catch (catchable body)
+  '(lambda (exception) 
+     (if (eq? exception (quote ,catchable))
+       ,body ; execute body
+     ; else
+       (super-throw exception)
+     )
+   )
+)
+
+; usage
+(try 
+  (begin  
+    (print "Inner")
+    (try 
+      (throw Exception)
+    (catch Exception
+      (print "Caught"))
+    ) 
   )
+(catch ExceptionTwo
+  (print "Caught ExceptionTwo")
 ))
-
-
-
-(try-catch
-  (lambda (throw)
-    (print "inside lambda")
-    (throw "My Exception")
-    (print "after exception"))
     
-  (lambda (e)
-    (print (+ "caught exception: " e))))
